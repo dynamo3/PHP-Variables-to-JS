281c8f99d57369c913abf972ce10d4c0064f77dc
Brad Westfall <brad@azpixels.com>
2014-06-05 16:18:20 -0700

    init

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include('payload.php');
+
+Payload::$values['foo'] = 'bar';
+
+include('view.php');
---
+++ view.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<body>
+
+	<script>
+		var payload = <?php echo Payload::render(); ?>
+	</script>
+
+</body>
+</html>
